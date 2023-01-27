35ea02f7e362445b83b07612160e0059d89f0e94
Manuel Escobar <manescob@fiu.edu>
2023-01-27 12:23:48 -0500

    encoder add-on

--- Prototype1/src/main.cpp
+++ Prototype1/src/main.cpp
@@ -6,12 +6,16 @@ class Setup {
         const uint8_t Relay2 = 10;
         const uint8_t Relay3 = 8;
         const uint8_t Relay4 = 7;
+        const uint8_t EncoderPort = 5;
 
         void Initialize(){
+            Serial.begin(115200);
+
             pinMode(Relay1, OUTPUT);
             pinMode(Relay2, OUTPUT);
             pinMode(Relay3, OUTPUT);
             pinMode(Relay4, OUTPUT);
+            pinMode(EncoderPort, INPUT);
 
             digitalWrite(Relay1, HIGH);
             digitalWrite(Relay2, HIGH);
@@ -27,17 +31,26 @@ class Setup {
         }
 };
 class HardwareControl{
-    
-};
+    public:
+        unsigned long Position = 0;
 
+        uint16_t EncoderPosition(const uint8_t &GPIO_ATTACHED_TO_ENCODER){
+            Position = pulseIn(GPIO_ATTACHED_TO_ENCODER, HIGH); 
+            return Position;
+        }
+
+    private:
+
+
+};
 
 void setup() {
     Setup OffRiser;
+    HardwareControl Encoder;
     OffRiser.Initialize();
-
     while (1)
-    {
-        
+    {   
+        Serial.println(Encoder.EncoderPosition(OffRiser.EncoderPort));
     }
 }
 
