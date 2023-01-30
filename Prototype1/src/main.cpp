b4970b800445754d01338c507727761056b90bd4
Manuel Escobar <mesco106@fiu.edu>
2023-01-30 00:03:51 -0500

    code improvement

--- Prototype1/src/main.cpp
+++ Prototype1/src/main.cpp
@@ -1,77 +1,49 @@
 #include "Arduino.h"
-#include "Vector.h"
 
-class Setup {
+using Pin = uint8_t;
+
+class RelayStripSetup{
     public:
-        const uint8_t Relay1 = 9;
-        const uint8_t Relay2 = 10;
-        const uint8_t Relay3 = 8;
-        const uint8_t Relay4 = 7;
-        const uint8_t EncoderPort = 5;
-
-        void Initialize(){
-            Serial.begin(115200);
-
-            pinMode(Relay1, OUTPUT);
-            pinMode(Relay2, OUTPUT);
-            pinMode(Relay3, OUTPUT);
-            pinMode(Relay4, OUTPUT);
-            pinMode(EncoderPort, INPUT);
-
-            digitalWrite(Relay1, HIGH);
-            digitalWrite(Relay2, HIGH);
-            digitalWrite(Relay3, HIGH);
-            digitalWrite(Relay4, HIGH);
+        RelayStripSetup(Pin relay1, Pin relay2, Pin relay3, Pin relay4){
+            m_relay1 = relay1;
+            m_relay2 = relay2;
+            m_relay3 = relay3;
+            m_relay4 = relay4;
+            pinMode(m_relay1, OUTPUT);
+            pinMode(m_relay2, OUTPUT);
+            pinMode(m_relay3, OUTPUT);
+            pinMode(m_relay4, OUTPUT);
         }
 
-        void toggle(const uint8_t &GPIO_PIN, int time){
-            digitalWrite(GPIO_PIN, LOW);
-            delay(time);
-            digitalWrite(GPIO_PIN, HIGH);
-            delay(time);
-        }
-};
-class HardwareControl{
-    public:
-    
-        uint16_t Position = 0;
-        unsigned long timer = micros();
-        unsigned long DeltaTime = 0;
-        unsigned long previousTime = 0;
-
-        uint16_t EncoderPosition(const uint8_t &GPIO_ATTACHED_TO_ENCODER){
-            Position = pulseIn(GPIO_ATTACHED_TO_ENCODER, HIGH); 
-            return Position;
-        }
-
-        void TurnRight(){
-            digitalWrite(Setup->Relay1, HIGH);
-            digitalWrite(Relay2, HIGH);
-            digitalWrite(Relay3, HIGH);
-            digitalWrite(Relay4, HIGH);
-        }
-
-        void TurnShaft(bool Direction){
-            //if (Direction == True)
-
-        }
-        void delay(uint64_t time){
-
-        }
-
-    
-
-
+    private:
+        Pin m_relay1;
+        Pin m_relay2;
+        Pin m_relay3;
+        Pin m_relay4;
 };
 
-void setup() {
-    Setup OffRiser;
-    HardwareControl Encoder;
-    OffRiser.Initialize();
-    while (1)
-    {   
-        Serial.println(Encoder.EncoderPosition(OffRiser.EncoderPort));
-    }
+class LinearActuators{
+    public:
+        //NOTE: Ask boss if I can set the member variable ousite the classic declaration.
+        void CounterClockwiseTurn(RelayStripSetup relayStripSetup, long executionTime){
+            m_executionTime = executionTime;
+            m_relayStripSetup = relayStripSetup;
+            digitalWrite(m_relayStripSetup.m_relay1, HIGH);
+        }
+        
+
+
+    private:
+    long m_executionTime;
+    RelayStripSetup m_relayStripSetup;
+};
+
+void setup(){
+    RelayStripSetup(1,2,3,4);
+    LinearActuators Actuators();
+
+    Actuators.CounterClockwiseTurn()
+
 }
 
-void loop() {}
+void loop(){}
